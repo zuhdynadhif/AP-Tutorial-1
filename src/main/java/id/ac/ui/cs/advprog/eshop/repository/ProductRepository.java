e4e58b613103c0bd851f0c6a4d8afe3aa471a714
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-02-02 21:34:34 +0700

    fix(repository): logic of productId
    set Id for product

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,12 +6,14 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        product.setProductId(String.valueOf(UUID.randomUUID()));
         productData.add(product);
         return product;
     }
