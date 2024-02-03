bdda5bc124807b250e263c603504bc60755e38f5
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-02-03 14:38:15 +0700

    test(model): attributes check

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -0,0 +1,33 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+    Product product;
+
+    @BeforeEach
+    void setUp() {
+        product = new Product();
+        this.product.setProductId("kdasl3432-234hlk2-2kljklrj-lkwje-lksefs");
+        this.product.setProductName("Sampo Cap Bambang");
+        this.product.setProductQuantity(100);
+    }
+
+    @Test
+    void testGetProductId() {
+        assertEquals("kdasl3432-234hlk2-2kljklrj-lkwje-lksefs", this.product.getProductId());
+    }
+
+    @Test
+    void testGetProductName() {
+        assertEquals("Sampo Cap Bambang", this.product.getProductName());
+    }
+
+    @Test
+    void testGetProductQuantity() {
+        assertEquals(100, this.product.getProductQuantity());
+    }
+}
