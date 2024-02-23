e43b667759005ed15fecdd76d2291c97854b8a3b
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-02-23 10:15:02 +0700

    test(mainApp): add main() test

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -3,11 +3,18 @@ package id.ac.ui.cs.advprog.eshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class EshopApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void mainTest(){
+        assertDoesNotThrow(() -> EshopApplication.main(new String[]{}));
     }
 
 }
