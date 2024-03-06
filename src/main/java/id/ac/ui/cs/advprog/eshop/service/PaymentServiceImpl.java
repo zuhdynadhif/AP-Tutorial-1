c6d7e08bbac431f27725d00459f7c6436727a62d
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-03-06 20:52:26 +0700

    [RED] feat(service): Implement PaymentService

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -21,21 +21,36 @@ public class PaymentServiceImpl implements PaymentService {
     private OrderRepository orderRepository;
     @Override
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        return null;
+        Payment payment = new Payment(order.getId(), method, paymentData);
+        paymentRepository.save(payment);
+        return payment;
     }
 
     @Override
     public Payment setStatus(Payment payment, String status) {
-        return null;
+        Order order = orderRepository.findById(payment.getId());
+        if (order != null){
+            if (status.equals(PaymentStatus.SUCCESS.getValue())){
+                order.setStatus(OrderStatus.SUCCESS.getValue());
+            } else if (status.equals(PaymentStatus.REJECTED.getValue())){
+                order.setStatus(OrderStatus.FAILED.getValue());
+            }
+            orderRepository.save(order);
+            payment.setStatus(status);
+            paymentRepository.save(payment);
+        } else {
+            throw new NoSuchElementException("Invalid payment");
+        }
+        return payment;
     }
 
     @Override
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
 
     @Override
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.findAll();
     }
 }
