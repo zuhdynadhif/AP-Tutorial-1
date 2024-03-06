3a9c1f147259569937392e4d2b7f4fe426dd8e10
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-03-06 18:58:18 +0700

    [GREEN] feat(model): Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -15,11 +15,30 @@ public class Payment {
     Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.status = OrderStatus.WAITING_PAYMENT.getValue();
+        if (paymentData.isEmpty()) {
+            throw new IllegalArgumentException("Payment data cannot be empty");
+        } else {
+            this.paymentData = paymentData;
+        }
     }
 
     public Payment(String id, String method, Map<String, String> paymentData, String status) {
+        this(id, method, paymentData);
+        if (OrderStatus.contains(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException("Invalid status");
+        }
     }
 
     public void setStatus(String status) {
+        if (OrderStatus.contains(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException("Invalid status");
+        }
     }
 }
